8b392c0ef64f489078723e033f4311dee7b5a2ed
Jannik Theyssen <25151498+janniktheyssen@users.noreply.github.com>
2020-12-18 13:21:49 +0100

    Update notebook.jl

--- notebook.jl
+++ notebook.jl
@@ -1,1 +1,24 @@
-println("hello from binder")
+### A Pluto.jl notebook ###
+
+# %%
+using Markdown
+using InteractiveUtils
+
+macro bind(def, element)
+    quote
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
+        el
+    end
+end
+
+
+begin
+	using Plots
+	using FFTW
+	using PlutoUI
+	using Pkg
+end;
+# %%
+
+println("Hello")
