28c6ee097ac5f6ba8320adaf087c4788df10a681
Jannik Theyssen <25151498+janniktheyssen@users.noreply.github.com>
2020-12-18 13:34:56 +0100

    Update notebook.jl

--- notebook.jl
+++ notebook.jl
@@ -1,24 +1,76 @@
-### A Pluto.jl notebook ###
-
-# %%
-using Markdown
-using InteractiveUtils
-
-macro bind(def, element)
-    quote
-        local el = $(esc(element))
-        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
-        el
-    end
-end
-
-
-begin
-	using Plots
-	using FFTW
-	using PlutoUI
-	using Pkg
-end;
-# %%
-
-println("Hello")
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "using Markdown\n",
+    "using InteractiveUtils\n",
+    "using Plots\n",
+    "using FFTW\n",
+    "using PlutoUI\n",
+    "using Pkg"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "#  Part 1: Critical frequency\n",
+    "Let's look at **wave-speed**, **wavelength** and **wavenumber** in more detail.\n",
+    "\n",
+    "The different **wave speeds** involved\n",
+    "\n",
+    "$c_0 = 343 \\text{ m/s}$\n",
+    "\n",
+    "$c_B = \\sqrt[4]{\\frac{B}{\\rho h}\\omega^2}= \\sqrt[4]{\\frac{B}{\\rho h}} \\sqrt{\\omega} = \\Lambda  \\sqrt{f}$ "
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "Let's set up some parameters"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "c0 = 343\n",
+    "\n",
+    "B = 1000\n",
+    "rho = 10\n",
+    "S = 0.01\n",
+    "f = 0.1:1:1000\n",
+    "omega = 2 * pi * f;"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Julia 1.5.3",
+   "language": "julia",
+   "name": "julia-1.5"
+  },
+  "language_info": {
+   "file_extension": ".jl",
+   "mimetype": "application/julia",
+   "name": "julia",
+   "version": "1.5.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
